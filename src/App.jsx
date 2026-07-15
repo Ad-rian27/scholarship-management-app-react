92e4b565f4fa5db34d7d1b96d41a6b3cd491db57
Krishnagadha <krishnagadhaurakkat@gmail.com>
2026-07-15 12:02:29 +0530

    student view created

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './components/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import ViewStud from './components/ViewStud'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/view-students" element={<ViewStud />} />
       </Routes>
       </BrowserRouter>
     </>
